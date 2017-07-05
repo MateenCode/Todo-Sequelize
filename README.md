506a4137c699380e3bcefa5ee3c3fba5a0465c12
“mateen1222” <mateenkaziawebdev@gmail.com>
2017-07-05 11:43:33 -0500

    Delete added data fixed

---
+++ README.md
@@ -0,0 +1,11 @@
+# ToDo-List
+
+A ToDo app created using Express, Mustache, Mysql, Sequelize.
+
+This simple app lets you create a list and mark it complete on the page as well on database!
+
+
+## ScreenShot
+
+<img width="227" alt="screen shot 2017-07-05 at 3 06 24 am" src="https://user-images.githubusercontent.com/28902787/27855722-2d550a70-6131-11e7-8e46-b8299b51ac51.png">
+<img width="600" alt="screen shot 2017-07-05 at 3 05 42 am" src="https://user-images.githubusercontent.com/28902787/27855736-3826616a-6131-11e7-9ab4-3c5a66025357.png">
---
+++ app.js
@@ -0,0 +1,87 @@
+//packages
+
+const express = require('express');
+const app = express();
+const mustache = require('mustache-express');
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+const models = require('./models/')
+
+// engines and sets
+
+app.engine('mustache', mustache());
+app.set('views', './views');
+app.set('view engine', 'mustache');
+
+// validators, body parser, and server listing
+
+app.use(expressValidator());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.listen(3000 , function (){
+  console.log('THE SERVER IS UP AND RUNNING!')
+});
+
+// empty arrays to push info into
+
+// get info from input
+
+
+app.get('/', function (request, response) {
+  let incomplete= []
+  let complete = []
+
+  models.todolist.findAll({order: [['createdAt', 'DESC']]})
+  .then(function(todos){
+
+    for(let i=0; i<todos.length; i++){
+      if(todos[i].complete === true){
+        complete.push(todos[i])
+      }else{
+        incomplete.push(todos[i])
+      }
+    }
+  })
+  .then(function(){
+    response.render('index', {
+      pageTitle: 'To Do List',
+      incomplete: incomplete,
+      complete: complete
+    })
+  })
+});
+
+// post info into the to do list
+app.post('/', function (request, response) {
+  let title = request.body.todos
+  models.todolist.create({
+    task:title,
+    complete:false
+  }).then(function () {
+    response.redirect('/')
+  })
+});
+
+
+// complete info and push into new list
+app.post('/completed', function (request, response) {
+  models.todolist.update({
+    completed: true
+  }, {
+    where: {
+      id: request.body.id
+    }
+  }).then(function () {
+    response.redirect('/')
+  })
+})
+
+
+app.post('/delete', function (request, response) {
+  models.todolist.destroy({
+    where: {
+      id: request.body.id
+    }
+  }).then(function () {
+    response.redirect('/')
+  })
+})
